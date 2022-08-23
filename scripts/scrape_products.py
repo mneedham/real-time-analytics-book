75b1cb967bd07db703454a1ab7477fbfbcd47ac0
Mark Needham <m.h.needham@gmail.com>
2022-08-23 14:19:24 +0100

    products in csv file

--- scripts/scrape_products.py
+++ scripts/scrape_products.py
@@ -1,51 +1,58 @@
 from calendar import c
-import requests
 from bs4 import BeautifulSoup
 import glob
 import json
+import csv
 
-for file_name in glob.glob("raw_data/*"):
-    # print(file_name)
+with open("mysql/data/products.csv", "w") as products_file:
+    writer = csv.writer(products_file, delimiter=",")
 
-    with open(file_name, "r") as file:
-        soup = BeautifulSoup(file.read(), 'html.parser')
+    writer.writerow(["name", "description", "price", "category", "image"])
 
-        name_element = soup.select("h1 span")
+    for file_name in glob.glob("raw_data/*"):
+        # print(file_name)
 
-        if len(name_element) > 0:
-            name = soup.select("h1 span")[0].text
+        with open(file_name, "r") as file:
+            soup = BeautifulSoup(file.read(), 'html.parser')
 
-            description_element = soup.select("section#main-section-mobile div.p-text-description.lead p")
-            description = description_element[0].text if len(description_element) > 0  else ""
-            
-            category = [item.text for item in soup.select("ul.breadcrumb li a span")][-2]
-            image = soup.select("div.col-md-6 img.element-left-40")[0]["src"]
-            crusts = [item.text.strip() for item in soup.select("ul.ul-list.features-list.element-top-1 li")]
+            name_element = soup.select("h1 span")
 
-            crust_sizes_element = soup.select("div.col-md-12.col-xs-12 p.sub-description.lead")
-            crust_sizes = [item.strip() for item in crust_sizes_element[0].text.strip().split("|")] if len(crust_sizes_element) > 0  else ["regular", "medium", "large"]
+            if len(name_element) > 0:
+                name = soup.select("h1 span")[0].text
 
-            prices_table = [[col.text.strip() for col in item.select("th")] for item in soup.select("section#main-section table tr")]
-            prices = {item[0]: item[1] for item in prices_table}
+                description_element = soup.select("section#main-section-mobile div.p-text-description.lead p")
+                description = description_element[0].text if len(description_element) > 0  else ""
+                
+                category = [item.text for item in soup.select("ul.breadcrumb li a span")][-2]
+                image = soup.select("div.col-md-6 img.element-left-40")[0]["src"]
+                crusts = [item.text.strip() for item in soup.select("ul.ul-list.features-list.element-top-1 li")]
 
-            price = soup.select("section#main-section span.price.text-left")[0].text.strip()
+                crust_sizes_element = soup.select("div.col-md-12.col-xs-12 p.sub-description.lead")
+                crust_sizes = [item.strip() for item in crust_sizes_element[0].text.strip().split("|")] if len(crust_sizes_element) > 0  else ["regular", "medium", "large"]
 
-            if price == "":
-                continue
+                prices_table = [[col.text.strip() for col in item.select("th")] for item in soup.select("section#main-section table tr")]
+                prices = {item[0]: item[1] for item in prices_table}
 
-            item = {
-                "name": " ".join([word.capitalize() for word in name.split(" ")]),
-                "description": description,
-                "price": price,
-                "category": category,
-                "image": image
-            }
+                price = soup.select("section#main-section span.price.text-left")[0].text.strip()
 
-            if category != "beverages":
-                item = {**item, 
-                    "crusts": crusts,
-                    "crustSizes": crust_sizes,
-                    "prices": prices
+                if price == "":
+                    continue
+
+                item = {
+                    "name": " ".join([word.capitalize() for word in name.split(" ")]),
+                    "description": description,
+                    "price": price,
+                    "category": category,
+                    "image": image
                 }
 
-            print(json.dumps(item))
+                if category != "beverages":
+                    item = {**item, 
+                        "crusts": crusts,
+                        "crustSizes": crust_sizes,
+                        "prices": prices
+                    }
+
+                writer.writerow([item["name"], item["description"], item["price"], item["category"], item["image"]])
+
+                print(json.dumps(item))
