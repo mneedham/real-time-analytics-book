87bfe518c785dbb74d830254dc737ebfe68e8e07
Mark Needham <m.h.needham@gmail.com>
2022-09-05 17:02:49 +0100

    kafka streams, why do you not work :(

--- kafka-streams/src/main/java/pizzashop/OrderItemsProductsJoin.java
+++ kafka-streams/src/main/java/pizzashop/OrderItemsProductsJoin.java
@@ -14,6 +14,7 @@ import pizzashop.deser.JsonSerializer;
 import pizzashop.deser.OrderItemWithOrderIdSerde;
 import pizzashop.models.*;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -31,7 +32,7 @@ public class OrderItemsProductsJoin {
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "pizzashop-orderitems21" + System.currentTimeMillis());
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
                 System.getenv().getOrDefault("BOOTSTRAP_SERVER", "localhost:29092"));
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Bytes().getClass());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, OrderItemWithOrderIdSerde.class.getName());
 
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
@@ -44,29 +45,29 @@ public class OrderItemsProductsJoin {
 
         final Serde<Order> orderSerde = Serdes.serdeFrom(new JsonSerializer<>(),
                 new JsonDeserializer<>(Order.class));
-        final KStream<Bytes, Order> orders = builder.stream("orders5",
-                Consumed.with(Serdes.Bytes(), orderSerde));
+        final KStream<String, Order> orders = builder.stream("orders8",
+                Consumed.with(Serdes.String(), orderSerde));
 
-        KStream<Bytes, OrderItemWithOrderId> orderItems = orders.flatMap((KeyValueMapper<Bytes, Order, Iterable<KeyValue<Bytes, OrderItemWithOrderId>>>) (bytes, order) -> {
-            List<KeyValue<Bytes, OrderItemWithOrderId>> result = new ArrayList<>();
+        KStream<String, OrderItemWithOrderId> orderItems = orders.flatMap((KeyValueMapper<String, Order, Iterable<KeyValue<String, OrderItemWithOrderId>>>) (key, value) -> {
+            List<KeyValue<String, OrderItemWithOrderId>> result = new ArrayList<>();
 
-            for (OrderItem item : order.items) {
+            for (OrderItem item : value.items) {
                 OrderItemWithOrderId orderItemWithOrderId = new OrderItemWithOrderId();
-                orderItemWithOrderId.orderId = order.id;
+                orderItemWithOrderId.orderId = value.id;
                 orderItemWithOrderId.price = item.price;
                 orderItemWithOrderId.quantity = item.quantity;
                 orderItemWithOrderId.productId = item.productId;
-                result.add(new KeyValue(new Bytes(String.valueOf(item.productId).getBytes()), orderItemWithOrderId));
+                result.add(new KeyValue(String.valueOf(item.productId), orderItemWithOrderId));
             }
             return result;
         });
 
         final Serde<Product> productSerde = Serdes.serdeFrom(new JsonSerializer<>(),
                 new JsonDeserializer<>(Product.class));
-        final KTable<Bytes, Product> products = builder.table("products6",
-                Consumed.with(Serdes.Bytes(), productSerde));
+        final KTable<String, Product> products = builder.table("products6",
+                Consumed.with(Serdes.String(), productSerde));
 
-        KStream<Bytes, HydratedOrderItem> enrichedOrderItems = orderItems.leftJoin(products, (orderItem, product) -> {
+        KStream<String, HydratedOrderItem> enrichedOrderItems = orderItems.leftJoin(products, (orderItem, product) -> {
             HydratedOrderItem hydratedOrderItem = new HydratedOrderItem();
             hydratedOrderItem.product = product;
 
@@ -84,31 +85,30 @@ public class OrderItemsProductsJoin {
         final Serde<HydratedOrderItem> hydratedOrderItemsSerde = Serdes.serdeFrom(new JsonSerializer<>(),
                 new JsonDeserializer<>(HydratedOrderItem.class));
 
-//        enrichedOrderItems.to("enriched-order-items5", Produced.with(Serdes.Bytes(), hydratedOrderItemsSerde));
+        enrichedOrderItems.to("enriched-order-items8", Produced.with(Serdes.String(), hydratedOrderItemsSerde));
 
         final Serde<HydratedOrder> hydratedOrdersSerde = Serdes.serdeFrom(new JsonSerializer<>(),
                 new JsonDeserializer<>(HydratedOrder.class));
 
-        KTable<Bytes, HydratedOrder> hydratedOrders = enrichedOrderItems
-                .groupBy((key, value) -> new Bytes(value.orderId.getBytes()),
-                        Grouped.with(Serdes.Bytes(), hydratedOrderItemsSerde))
+        KTable<String, HydratedOrder> hydratedOrders = enrichedOrderItems
+                .groupBy((key, value) -> value.orderId,
+                        Grouped.with(Serdes.String(), hydratedOrderItemsSerde))
                 .aggregate(HydratedOrder::new, (key, value, aggregate) -> {
                     aggregate.addOrderItem(value);
                     return aggregate;
-                }, Materialized.with(Serdes.Bytes(), hydratedOrdersSerde));
+                }, Materialized.with(Serdes.String(), hydratedOrdersSerde));
 
         orders.peek((key, value) -> System.out.println("key = " + key + ", value = " + value));
-
-        KStream<Bytes, Order> rekeyedOrders = orders.selectKey((key, value) -> new Bytes(value.id.getBytes()));
-
+//
         hydratedOrders.toStream().peek((key, value) -> System.out.println("key = " + key + ", value = " + value));
 //
-//        hydratedOrders.toStream().to("rogue-orders6", Produced.with(Serdes.Bytes(), hydratedOrdersSerde));
+//        hydratedOrders.toStream().selectKey((key, value) -> new Bytes(key.getBytes())).to("hydrated-orders-testing-10", Produced.with(Serdes.Bytes(), hydratedOrdersSerde));
+//        orders.to("orders-testing-10", Produced.with(Serdes.String(), orderSerde));
 
         final Serde<CompleteOrder> completeOrderSerde = Serdes.serdeFrom(new JsonSerializer<>(),
                 new JsonDeserializer<>(CompleteOrder.class));
 
-        rekeyedOrders.leftJoin(hydratedOrders, (value1, value2) -> {
+        orders.join(hydratedOrders, (value1, value2) -> {
             CompleteOrder completeOrder = new CompleteOrder();
             completeOrder.id = value1.id;
             completeOrder.status = value1.status;
@@ -120,8 +120,8 @@ public class OrderItemsProductsJoin {
             }
             return completeOrder;
         })
-                .peek((key, value) -> System.out.println("key = " + key + ", value = " + value))
-                .to("enriched-orders6", Produced.with(Serdes.Bytes(), completeOrderSerde));
+//                .peek((key, value) -> System.out.println("key = " + key + ", value = " + value))
+                .to("enriched-orders9", Produced.with(Serdes.String(), completeOrderSerde));
 
         final KafkaStreams streams = new KafkaStreams(builder.build(), props);
         final CountDownLatch latch = new CountDownLatch(1);
