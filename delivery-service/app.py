e881a1c06ee9bf92aa45c08f3e0b6644e57afca4
Mark Needham <m.h.needham@gmail.com>
2022-11-02 16:27:46 +0000

    add delivery statuses

--- delivery-service/app.py
+++ delivery-service/app.py
@@ -3,6 +3,27 @@ import datetime
 import random
 from dateutil import parser
 from sortedcontainers import SortedList
+from mysql.connector import connect, Error
+
+
+from pyproj import Geod
+import pyproj
+import geopy.distance
+
+import os
+
+orderInterval      = 100
+mysqlHost          = os.environ.get("MYSQL_SERVER", "localhost")
+mysqlPort          = '3306'
+mysqlUser          = 'mysqluser'
+mysqlPass          = 'mysqlpw'
+
+
+connection = connect(
+    host=mysqlHost,
+    user=mysqlUser,
+    password=mysqlPass,
+)
 
 STATUSES = [
     # PLACED_ORDER
@@ -28,9 +49,40 @@ app = faust.App(
 
 orders_topic = app.topic('orders')
 orders_statuses_topic = app.topic('ordersStatuses')
-
+delivery_statuses_topic = app.topic('deliveryStatuses')
 
 other_statuses = SortedList(key=lambda x: x["updatedAt"])
+delivery_statuses = SortedList(key=lambda x: x["updatedAt"])
+
+def generate_delivery_statuses(order, last_status_time, shop_location, delivery_location, points_to_generate):
+    statuses = []
+    if points_to_generate > 0:
+        try:
+            extra_points = geoid.npts(shop_location[0], shop_location[1], delivery_location[0], delivery_location[1], points_to_generate)
+            for point in extra_points:    
+                next_status_time = last_status_time + datetime.timedelta(seconds=1)
+
+                statuses.append({
+                    "id": order["id"],
+                    "updatedAt": next_status_time.isoformat(),
+                    "deliveryLat": str(point[0]),
+                    "deliveryLon": str(point[1])
+                })
+                last_status_time = next_status_time
+        except pyproj.exceptions.GeodError as e:
+            print(e)
+
+    statuses.append({
+        "id": order["id"],
+        "updatedAt": (last_status_time + datetime.timedelta(seconds=1)).isoformat(),
+        "deliveryLat": str(delivery_location[0]),
+        "deliveryLon": str(delivery_location[1])
+    })
+    return statuses
+
+shop_location = (12.978268132410502, 77.59408889388118)
+driver_km_per_hour = 150
+geoid = Geod(ellps="WGS84")
 
 @app.agent(orders_topic)
 async def orders(stream):
@@ -45,7 +97,6 @@ async def orders(stream):
             key=order_status["id"],
             value=order_status
         )
-
 
         placed_order_time = parser.parse(order["createdAt"])
         last_status_time = placed_order_time
@@ -62,14 +113,53 @@ async def orders(stream):
             })
             last_status_time = next_status_time
 
+        cursor = connection.cursor()
+        cursor.execute("SELECT lat,lon FROM pizzashop.users WHERE id = %s", (order["userId"],))        
+        delivery_location = [(row[0], row[1]) for row in cursor][0]
+        cursor.close()
+
+        dist = geopy.distance.distance(shop_location, delivery_location).meters
+        minutes_to_deliver = (dist / (driver_km_per_hour * 1000)) * 60
+
+        next_status_time = last_status_time + datetime.timedelta(seconds=minutes_to_deliver*60)
+        other_statuses.add({
+            "id": order["id"],
+            "updatedAt": next_status_time.isoformat(),
+            "status": "DELIVERED"
+        })
+
+        delivery_statuses.add({
+            "id": order["id"],
+            "updatedAt": last_status_time.isoformat(),
+            "deliveryLat": str(shop_location[0]),
+            "deliveryLon": str(shop_location[1])
+        })
+
+        points_to_generate = minutes_to_deliver * 60
+
+        delivery_statuses.update(
+            generate_delivery_statuses(order, last_status_time, shop_location, delivery_location, points_to_generate)
+        )
+
 
 @app.timer(interval=10.0)
-async def every_10_seconds():
-    other_statuses_to_publish = [item for item in other_statuses if parser.parse(item["updatedAt"]) < datetime.datetime.now()]
-    for status in other_statuses_to_publish:
+async def order_status_poller():
+    statuses_to_publish = [item for item in other_statuses if parser.parse(item["updatedAt"]) < datetime.datetime.now()]
+    for status in statuses_to_publish:
         await orders_statuses_topic.send(
             key=status["id"],
             value=status
         )
 
         other_statuses.remove(status)
+
+@app.timer(interval=10.0)
+async def delivery_status_poller():
+    statuses_to_publish = [item for item in delivery_statuses if parser.parse(item["updatedAt"]) < datetime.datetime.now()]
+    for status in statuses_to_publish:
+        await delivery_statuses_topic.send(
+            key=status["id"],
+            value=status
+        )
+
+        delivery_statuses.remove(status)
